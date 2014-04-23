bc2b226f592c4b81e03edb27c0e5ca23354859a0
Cees-Jan <ceesjank@gmail.com>
2014-04-23 22:31:36 +0200

    Initial source commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# wyrihaximus/phergie-dns
+
+[Phergie](http://github.com/phergie/phergie-irc-bot-react/) plugin for Looking up IP&#039;s by hostnames.
+
+[![Build Status](https://secure.travis-ci.org/wyrihaximus/phergie-dns.png?branch=master)](http://travis-ci.org/wyrihaximus/phergie-dns)
+
+## Install
+
+The recommended method of installation is [through composer](http://getcomposer.org).
+
+```JSON
+{
+    "require": {
+        "wyrihaximus/phergie-dns": "dev-master"
+    }
+}
+```
+
+See Phergie documentation for more information on
+[installing and enabling plugins](https://github.com/phergie/phergie-irc-bot-react/wiki/Usage#plugins).
+
+## Configuration
+
+```php
+new \WyriHaximus\Phergie\Plugin\Dns\Plugin(array(
+
+
+
+))
+```
+
+## Tests
+
+To run the unit test suite:
+
+```
+curl -s https://getcomposer.org/installer | php
+php composer.phar install
+cd tests
+../vendor/bin/phpunit
+```
+
+## License
+
+Released under the BSD License. See `LICENSE`.
---
+++ src/Plugin.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ * This file is part of PhergieDns.
+ *
+ ** (c) 2013 - 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WyriHaximus\Phergie\Plugin\Dns;
+
+use Phergie\Irc\Bot\React\AbstractPlugin;
+use Phergie\Irc\Bot\React\EventQueueInterface;
+use Phergie\Irc\Plugin\React\Command\CommandEvent;
+use React\Dns\Resolver\Factory;
+
+/**
+ * Plugin for Looking up IP&#039;s by hostnames.
+ *
+ * @category Phergie
+ * @package WyriHaximus\Phergie\Plugin\Dns
+ */
+class Plugin extends AbstractPlugin
+{
+    protected $resolver;
+
+    protected $dnsServer = '8.8.8.8';
+    protected $command = 'dns';
+
+    /**
+     * Accepts plugin configuration.
+     *
+     * Supported keys:
+     *
+     *
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = array())
+    {
+        if (isset($config['dnsServer'])) {
+            $this->dnsServer = $config['dnsServer'];
+        }
+        if (isset($config['command'])) {
+            $this->command = $config['command'];
+        }
+    }
+
+    /**
+     *
+     *
+     * @return array
+     */
+    public function getSubscribedEvents()
+    {
+        return array(
+            'command.' . $this->command => 'handleDnsCommand',
+        );
+    }
+
+    public function handleDnsCommand(CommandEvent $event, EventQueueInterface $queue)
+    {
+        foreach ($event->getCustomParams() as $hostname) {
+            $message = $hostname . ': ';
+            $this->logger->debug('Looking up: ' . $hostname);
+            $this->getResolver()->resolve($hostname)->then(function ($ip) use ($event, $queue, $message) {
+                $message = $message . $ip;
+                $this->logger->debug($message);
+                foreach ($event->getTargets() as $target) {
+                    $queue->ircPrivmsg($target, $message);
+                }
+            });
+        }
+    }
+
+    public function getResolver() {
+        if ($this->resolver !== null) {
+            return $this->resolver;
+        }
+
+        $factory = new Factory();
+        $this->resolver = $factory->createCached($this->dnsServer, $this->loop);
+
+        return $this->resolver;
+    }
+
+    
+}
